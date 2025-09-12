5052acb8145ab90c1398e635530a99a5287323b9
Luciano Soares <1084292+soaresluciano@users.noreply.github.com>
2025-09-12 11:13:27 +0200

    Add additional Flatpak installations

---
+++ myflatpaks.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+flatpak install cc.arduino.IDE2 #Arduino IDE v2 
+flatpak install com.anydesk.Anydesk #AnyDesk
+flatpak install com.bitwarden.desktop #Bitwarden
+flatpak install com.heroicgameslauncher.hgl #Heroic Games Launcher
+flatpak install com.usebottles.bottles #Bottles
+flatpak install io.github.peazip.PeaZip #PeaZip
+flatpak install io.podman_desktop.PodmanDesktop #Podman Desktop
+flatpak install md.obsidian.Obsidian #Obsidian
+flatpak install net.davidotek.pupgui2 #ProtonUp-Qt
+flatpak install org.localsend.localsend_app #KeyStore Explorer
+flatpak install org.localsend.localsend_app #LocalSend
