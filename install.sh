7175fcd29ac28238139b59e0e235b18b8c043bc1
Luciano Soares <1084292+soaresluciano@users.noreply.github.com>
2025-09-16 19:24:12 +0200

    Enhance install script with mirrorlist and packages

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 # 2.1 Select the mirrors
+echo -n ":: Creating mirrorlist"
 reflector --latest 200 --sort rate --country France,German,Belgium,Netherlands --save /etc/pacman.d/mirrorlist
 
 # 2.2 Install essential packages
+echo -n ":: Installing essesitial packages"
 pacstrap -K /mnt base linux linux-firmware
 pacstrap -K /mnt \
     sudo \
@@ -23,4 +25,12 @@ read -p "The CPU brand is: (amd/intel)" CPU
 pacstrap -K /mnt "$CPU-ucode"
 
 # 3.1 Fstab
+echo -n ":: Creating fstab"
 genfstab -U /mnt >> /mnt/etc/fstab
+
+echo -n ":: Done!"
+echo -n "- You can execute:"
+echo -n "arch-chroot /mnt"
+echo -n ""
+echo -n "- and then:"
+echo -n "wget https://raw.githubusercontent.com/soaresluciano/arch/refs/heads/main/setup.sh"
