ee06b271c770f492764eecb3f15ec01f5389d66a
Luciano Soares <1084292+soaresluciano@users.noreply.github.com>
2025-09-16 21:40:18 +0200

    Remove extra software installation section
    Removed the extra software installation prompt and related commands.

--- myapps.sh
+++ myapps.sh
@@ -15,33 +15,3 @@ sudo pacman -S --needed \
   gnome-firmware \
   pavucontrol \
   vlc
-
-# extra software
-read -p "Do you want to install extra software? (y/n) " -n 1 -r
-echo
-if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-  echo "Skipping extra software installation."
-  exit 0
-fi
-echo "Installing extra software..."
-
-sudo pacman -S --needed \
-  bleachbit \
-  clamav \
-  diffuse \
-  distrobox \
-  geany \
-  inkscape \
-  kleopatra \
-  lazygit \
-  libreoffice-still \
-  qbittorrent \
-  obsidian \
-  openssh \
-  podman-desktop \
-  rclone \
-  remmina \
-  sqlitebrowser \
-  steam \
-  tailscale \
-  wine
