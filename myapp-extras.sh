aafd5b6d49ddcf1aef07818a5444d752b7d6d95f
Luciano Soares <1084292+soaresluciano@users.noreply.github.com>
2025-09-16 21:18:28 +0200

    Create myapp-extras.sh

---
+++ myapp-extras.sh
@@ -0,0 +1,19 @@
+sudo pacman -S --needed \
+  bleachbit \
+  clamav \
+  diffuse \
+  distrobox \
+  geany \
+  inkscape \
+  kleopatra \
+  lazygit \
+  libreoffice-still \
+  qbittorrent \
+  obsidian \
+  openssh \
+  podman-desktop \
+  rclone \
+  remmina \
+  sqlitebrowser \
+  tailscale \
+  wine
