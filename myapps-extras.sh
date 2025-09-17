0b83197ec7c74bcb3f06924093520147afad294b
Luciano Soares <1084292+soaresluciano@users.noreply.github.com>
2025-09-17 18:15:30 +0200

    Update myapps-extras.sh

--- myapps-extras.sh
+++ myapps-extras.sh
@@ -2,18 +2,12 @@ sudo pacman -S --needed \
   bleachbit \
   clamav \
   diffuse \
-  distrobox \
   geany \
-  inkscape \
-  kleopatra \
   lazygit \
   libreoffice-still \
-  qbittorrent \
   obsidian \
   openssh \
-  podman-desktop \
   rclone \
   remmina \
-  sqlitebrowser \
   tailscale \
   wine
