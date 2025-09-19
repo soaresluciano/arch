6a17ea868f0571a50e072e2861bfe9055447a5b4
Luciano Soares <1084292+soaresluciano@users.noreply.github.com>
2025-09-19 09:51:05 +0200

    Update myapps.sh to modify software installation
    Removed several software packages from installation list.

--- myapps.sh
+++ myapps.sh
@@ -2,14 +2,8 @@
 
 echo "Installing basic software..."
 sudo pacman -S --needed \
-  7zip \
   bat \
-  bitwarden \
-  btop \
   chezmoi \
   exa \
-  firefox \
   flatpak \
-  fzf \
-  pavucontrol
-  
+  fzf
