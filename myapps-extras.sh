9f0910495e91866e0d054979e20cbce712983e69
Luciano Soares <1084292+soaresluciano@users.noreply.github.com>
2025-09-19 09:53:55 +0200

    Remove openssh from package installation list
    Removed 'openssh' from the list of packages to install.

--- myapps-extras.sh
+++ myapps-extras.sh
@@ -1,13 +1,19 @@
 sudo pacman -S --needed \
+  7zip \
+  bitwarden \
   bleachbit \
+  btop \
   clamav \
   diffuse \
+  firefox \
   geany \
+  gnome-firmware \
   lazygit \
   libreoffice-still \
   obsidian \
-  openssh \
+  pavucontrol \
   rclone \
   remmina \
   tailscale \
+  vlc \
   wine
