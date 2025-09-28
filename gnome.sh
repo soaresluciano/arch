069f750fa40f843f3c66be340d1ebb1b77c5bb06
Luciano Soares <1084292+soaresluciano@users.noreply.github.com>
2025-09-28 18:11:52 +0200

    gnome

--- gnome.sh
+++ gnome.sh
@@ -1,1 +1,13 @@
-pacman -S gnome gnome-tweaks gnome-browser-connector ufw
+pacman -S gnome gnome-tweaks gnome-browser-connector ufw cups system-config-printer nss-mdns
+
+#enable GDM
+sudo systemctl enable --now gdm.service
+
+#enable bluetooth
+sudo systemctl enable --now bluetooth.service
+
+#enable CUPS
+sudo systemctl enable --now cups.service
+
+#enable multicast DNS / DNS Service Discovery
+sudo systemctl enable --now avahi-daemon.service
